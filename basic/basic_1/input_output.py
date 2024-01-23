6cd1d5f7f1bffd4a31bb681f46d9d714b54918ce
Irfan <Irfan_Shaik@epam.com>
2024-01-23 15:31:27 +0530

    added files

---
+++ basic/basic_1/input_output.py
@@ -0,0 +1,5 @@
+print("This article belongs to Takeuforward", end='-->')
+print("tuf", "striver", "India", sep='& ')
+
+a = list(map(int, input().split(',')))  # input value must be integer with base 10
+print(a)
